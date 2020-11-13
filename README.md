6c4a46895c4ce93293dbf02ee2ba864d0c9021ad
Geoff Nix <FullStackGeoff@gmail.com>
2020-11-12 22:26:10 -0500

    GET and POST routes are complete

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # express-barber
+
+
+### Intro
+This is a practice exercise to use express.js to create a server and establish routes
+
+### Steps for set-up
+1. create a server.js file
+2. npm init -y
+3. npm install express
+
+### Steps to build server
+1. require express
+2. create an instance of express
+3. set the PORT. (use process.env.PORT || PORT)
+4. listen to the port
+5. add middleware
+
+### Build Routes
+* build a test route, `/api/config`
+
+### View Routes
+* always going to be get routes
+* sends back HTML for the browser to parse and display
+* use `res.sendFile` to send back a previously built HTML
+
+#### To Send Back an HTML file
+1. Build a get route
+2. Call `res.sendFile` 
+3. Use `path.join()` to combine the _dirname w/the file name(`npm install path`)
+
+### API Routes
+* Resource-driven API Development
+
+1. GET "/api/resources" returns a collection of resources
+2. GET "/api/resources/:id" returns a single resource
+3. POST "/api/resources" allows the user to create a new resources
---
+++ server.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+	res.sendFile(path.join(__dirname, "/views/index.html"));
+});
+
+app.get("/api/config", (req, res) => {
+	res.json({
+		success: true,
+	});
+});
+
+const haircuts = [
+    {
+        name: "Quo Vidas",
+        price: "$25"
+    },
+    {
+        name: "Afro",
+        price: "$40"
+    },
+    {
+        name: "Low Fade",
+        price: "$30"
+    },
+    {
+        name: "Mohawk",
+        price: "$45"
+    },
+];
+
+app.get("/api/haircuts", (req, res) => {
+    res.json(haircuts);
+});
+
+app.get("/api/haircuts/:name", (req,res) => {
+    for(let i = 0; i < haircuts.length; i++) {
+        if(haircuts[i].name === req.params.name){
+            return res.json(haircuts[i]);
+        }
+    }
+});
+
+app.post("/api/haircuts", (req, res) => {
+    haircuts.push(req.body);
+    res.json(haircuts);
+});
+
+
+app.listen(PORT, () => {
+	console.log(`You are listening on port http://localhost:${PORT}`);
+});
